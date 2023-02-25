01c191040f81f8eb7716ae350b1d7c6a1b07a360
Kerstin <kerstinweibeler@gmail.com>
2023-02-25 12:52:33 +0000

    test: Add test case for models

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+from .models import Item
+
+
+class TestModals(TestCase):
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test ToDo Item')
+        self.assertFalse(item.done)
